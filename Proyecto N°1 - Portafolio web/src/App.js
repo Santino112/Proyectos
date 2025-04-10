1ce64cdaf8a4b6ccd7359c3533edbf8c1d5225cd
Santino112 <kisumi600@gmail.com>
2025-04-10 20:27:17 -0300

    Commit

--- Proyecto N°1 - Portafolio web/src/App.js
+++ Proyecto N°1 - Portafolio web/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Hola a  todos</h1>
+      <h2>Tetas</h2>
       <buttoN>Apretame</buttoN>
     </div>
   );
