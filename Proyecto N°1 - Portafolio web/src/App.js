8198ce0a87b81c69b4843b71bf34c7dbf6b33482
Santino112 <kisumi600@gmail.com>
2025-04-10 21:59:35 -0300

    passadaso

--- Proyecto N°1 - Portafolio web/src/App.js
+++ Proyecto N°1 - Portafolio web/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
+function alerta(){
+  alert("Sos un trolaso!");
+}
+
 function App() {
   return (
-    <div>
+    <div className='Contenedor1'>
       <h1>Hola a  todos</h1>
       <h2>Tetas</h2>
-      <buttoN>Apretame</buttoN>
+      <buttoN onClick={alerta}>Apretame</buttoN>
+      <section></section>
     </div>
   );
 }
